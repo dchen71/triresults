e8ef12847ba8387a886ecf41573b64a5e328becf
Dan <dchen12812@gmail.com>
2016-04-14 00:34:36 -0700

    fixed link with mongo

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module Triresults
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    Mongoid.load!('./config/mongoid.yml')
   end
 end
