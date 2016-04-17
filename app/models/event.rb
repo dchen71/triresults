8d8cf30dd85b3e7d365783efa0145e1d908df684
Dan <dchen12812@gmail.com>
2016-04-16 17:55:17 -0700

    added fields

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event
   include Mongoid::Document
+
+  field :o, as: :order, type: Integer
+  field :n, as: :name, type: String
+  field :d, as: :distance, type: Float
+  field :u, as: :units, type: String
 end
