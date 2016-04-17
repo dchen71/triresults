b1149dbb1d77662a611dce408d866d4962a2abda
Dan <dchen12812@gmail.com>
2016-04-16 18:08:42 -0700

    added relationship embed

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,6 +6,8 @@ class Event
   field :d, as: :distance, type: Float
   field :u, as: :units, type: String
 
+  embedded_in :parent, polymorphic: true, touch: true
+
   #Convert to meters
   def meters
   	if self.d
--- app/models/race.rb
+++ app/models/race.rb
@@ -5,4 +5,6 @@ class Race
   field :n, as: :name, type: String
   field :date, type: Date
   field :loc, as: :location, type: Address
+
+  embeds_many :events, as: :parent, order: [:order.asc]
 end
