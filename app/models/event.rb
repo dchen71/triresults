a1dee5521b050ff1fb6f144c27e95d5f23e321d1
Dan <dchen12812@gmail.com>
2016-04-16 17:59:40 -0700

    added meters and miles methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,28 @@ class Event
   field :n, as: :name, type: String
   field :d, as: :distance, type: Float
   field :u, as: :units, type: String
+
+  #Convert to meters
+  def meters
+  	if self.d
+  		case self.u
+  		when "meters" then self.d
+  		when "kilometers" then self.d * 1000
+  		when "yards" then self.d * 0.9144
+  		when "miles" then self.d * 1609.34
+  		end
+  	end
+  end
+
+  #Convert to miles
+  def miles
+  	if self.d
+  		case self.u
+  		when "meters" then self.d * 0.000621371
+  		when "kilometers" then self.d * 0.621371
+  		when "yards" then self.d * 0.000568182
+  		when "miles" then self.d
+  		end
+  	end
+  end
 end
