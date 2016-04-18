8358a1ff31f81bb9669014059770a5f99d1d19f5
Dan <dchen12812@gmail.com>
2016-04-17 22:14:43 -0700

    updated leg result with passing test case powers

--- app/models/leg_result.rb
+++ app/models/leg_result.rb
@@ -1,3 +1,19 @@
 class LegResult
   include Mongoid::Document
+
+  field :secs, type: Float
+
+  def calc_ave
+  	#subclasses will calc event-specific ave
+  end
+
+  after_initialize do |doc|
+  	calc_ave
+  end
+
+  #Calc ave after manuallly updating self with provided value
+  def secs=value
+	self[:secs] = value
+	calc_ave
+  end
 end
