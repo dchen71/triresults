96121a34897de25c0177cfc08c529fafe9cf727e
Dan <dchen12812@gmail.com>
2016-04-17 23:32:29 -0700

    added fields to result subclasses

--- app/models/run_result.rb
+++ app/models/run_result.rb
@@ -1,3 +1,12 @@
 class RunResult < LegResult
   include Mongoid::Document
+
+  field :mmile, as: :minute_mile, type: Float
+
+   def calc_ave
+   	if event && secs
+    	meters = event.meters
+    	self.my_ave=meters.nil? ? nil : 12.1
+   	end
+   end
 end
--- app/models/swim_result.rb
+++ app/models/swim_result.rb
@@ -1,3 +1,12 @@
 class SwimResult < LegResult
   include Mongoid::Document
+
+  field :pace_100, type: Float
+
+  def calc_ave
+  	if event && secs
+	  meters = event.meters
+	  self.my_ave=meters.nil? ? nil : 12.1
+  	end
+  end
 end
